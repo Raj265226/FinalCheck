b218a4627f13fc44d64298894f5ef97ba31a3b00
Rohit Santra <92876456+Raj265226@users.noreply.github.com>
2026-03-05 05:10:32 +0530

    Add Selenium script for link status checking
    Implemented a Selenium script to check the status of links on a webpage and handle multiple windows.

---
+++ Code_2026.py
@@ -0,0 +1,45 @@
+# tab switch and broken link
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+driver.maximize_window()
+parent_window = driver.current_window_handle
+links = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//a[@href]')))
+for link in links:
+    link.click()
+    if not link.get_attribute('href'):                                     # python treats '',0,False,None as a False
+        print('no url found')
+    else:
+        try:
+            response = requests.get(link.get_attribute('href'))
+            if response.status_code == 200:
+                print('url ->',link.get_attribute('href'),' is working perfectly and status -> ',response.status_code)
+            else:
+                print('url ->',link.get_attribute('href'),' is not working perfectly and status -> ',response.status_code)
+        except requests.exceptions.RequestException as e:                 # It handles all request related exceptions
+            print('Error',e)
+
+windows = driver.window_handles
+for window in windows:
+    if window != parent_window:
+        driver.switch_to.window(window)
+        print(driver.title)
+        driver.close()
+driver.switch_to.window(parent_window)
+WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(1))
+
+
+
+
+
+
+
+
